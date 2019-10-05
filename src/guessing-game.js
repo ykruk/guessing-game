1bba1475febca222f4c3319ca8b8d0c6fe5b83dc
ykruk <yulka.kr@gmail.com>
2019-10-06 00:06:16 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,27 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.array = [];
+        for (let i = min; i <= max; i++) {
+            this.array.push(i);
+        }
+        this.startIndex = 0;
+        this.endIndex = this.array.length - 1;
+        this.middleIndex = Math.ceil((this.startIndex + this.endIndex) / 2);
     }
 
     guess() {
-
+        return this.array[this.middleIndex];
     }
 
     lower() {
-
+        this.endIndex = this.middleIndex;
+        this.middleIndex = Math.ceil((this.startIndex + this.endIndex) / 2);
     }
 
     greater() {
-
+        this.startIndex = this.middleIndex;
+        this.middleIndex = Math.ceil((this.startIndex + this.endIndex) / 2);
     }
 }
 
